86a495ac7ca5b519ce24ca74413875f444059074
ellirp <78500732+ellirp@users.noreply.github.com>
2023-04-26 17:22:14 -0400

    Updated Version

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -2,19 +2,33 @@ class MovieRepository:
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        return None
+        session = self.Session()
+        movies = session.query(movies).all()
+        session.close()
+        return movies
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
-        return None
+        session = self.Session()
+        movie = session.query(movie).filter_by(movie_id=movie_id).first()
+        session.close()
+        return movie
 
     def create_movie(self, title, director, rating):
         # TODO create a new movie in the DB
-        return None
+        session = self.Session()
+        movie = movie(title=title, director=director, rating=rating)
+        session.add(movie)
+        session.commit()
+        session.close()
+        return movie
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
+        session = self.Session()
+        movies = session.query(movies).filter(movies.title.ilike(f'%{title}%')).all()
+        session.close()
+        return movies
 
 
 # Singleton to be used in other modules
